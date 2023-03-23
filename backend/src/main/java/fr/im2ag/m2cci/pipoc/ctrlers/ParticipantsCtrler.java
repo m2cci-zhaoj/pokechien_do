480325b819f766482bedb12b7395c462d190070b
Philippe Genoud <Philippe.Genoud@univ-grenoble-alpes.fr>
2023-03-24 00:47:41 +0100

    utilisation de bootstrap et possibilité d'afficher un dialogue quand clic sur un stop

--- backend/src/main/java/fr/im2ag/m2cci/pipoc/ctrlers/ParticipantsCtrler.java
+++ backend/src/main/java/fr/im2ag/m2cci/pipoc/ctrlers/ParticipantsCtrler.java
@@ -1,21 +1,16 @@
 package fr.im2ag.m2cci.pipoc.ctrlers;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import fr.im2ag.m2cci.pipoc.dto.Participant;
 
